05bfebbbc5a53adb118f3e3fc46c72c91b00b974
Eric Kloss <eric.kloss@sandstorm.de>
2018-12-07 00:46:35 +0100

    echo decrypt

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,5 +2,6 @@
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ];
 then
+    echo "Decrypt GPG secret from $ENCRYPTED_GPG_KEY_LOCATION to $GPG_KEY_LOCATION"
     openssl aes-256-cbc -K $encrypted_42338e236d2b_key -iv $encrypted_42338e236d2b_iv -in $ENCRYPTED_GPG_KEY_LOCATION -out $GPG_KEY_LOCATION -d
 fi
