16661d3bb70e40a6c36ce081062733a4706eb019
Eric Kloss <eric.kloss@sandstorm.de>
2018-12-07 02:03:47 +0100

    maven-publish

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,31 +2,11 @@
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ];
 then
-    echo "Sign Archives - annotations"
-    ./gradlew :annotations:signArchives
-    echo "Upload Archives - annotations"
-    ./gradlew :annotations:uploadArchives
-    echo "Sign Archives - core"
-    ./gradlew :core:signArchives
-    echo "Upload Archives - core"
-    ./gradlew :core:uploadArchives
-    echo "Sign Archives - processor-spring"
-    ./gradlew :processor-spring:signArchives
-    echo "Upload Archives - processor-spring"
-    ./gradlew :processor-spring:uploadArchives
-    echo "Sign Archives - processor-standalone"
-    ./gradlew :processor-standalone:signArchives
-    echo "Upload Archives - processor-standalone"
-    ./gradlew :processor-standalone:uploadArchives
+    echo "Sign and Upload Archives"
+    ./gradlew :annotations:publish :core:publish :processor-standalone:publish :processor-spring:publish
     if [ "$MANUAL_RELEASE_TRIGGERED" = "true" ];
     then
-        echo "Promote repository - annotations"
-        ./gradlew :annotations:closeAndReleaseRepository
-        echo "Promote repository - core"
-        ./gradlew :core:closeAndReleaseRepository
-        echo "Promote repository - processor-spring"
-        ./gradlew :processor-spring:closeAndReleaseRepository
-        echo "Promote repository - processor-standalone"
-        ./gradlew :processor-standalone:closeAndReleaseRepository
+        echo "Promote repository"
+        ./gradlew :annotations:closeAndReleaseRepository :core:closeAndReleaseRepository :processor-spring:closeAndReleaseRepository :processor-standalone:closeAndReleaseRepository
     fi
 fi
