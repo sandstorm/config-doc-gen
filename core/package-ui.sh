90bef8e210e0eda8cba576d544c1025ea3c085af
Eric Kloss <eric.kloss@sandstorm.de>
2019-02-17 12:45:07 +0100

    BUGFIX: react ui package via docker

--- core/package-ui.sh
+++ core/package-ui.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 pushd ../local-dev/react-ui
+./setup.sh
 ./package-ui.sh
 popd
 
--- local-dev/react-ui/_run-container.sh
+++ local-dev/react-ui/_run-container.sh
@@ -1,24 +1,31 @@
 #!/usr/bin/env bash
 
-echo "running command: $COMMAND"
+echo "initializing command: $COMMAND"
 
 echo "{\"version\":\"$(git rev-parse --short HEAD)\"}" > ../../react-ui/src/VERSION.json
+echo "written VERSION.json"
 
-docker run -it --rm \
-    -p 3000:3000 \
-    -e "COMMAND=$COMMAND" \
-    -v "$(pwd)/../../react-ui/.storybook/:/app/.storybook/:ro,cached" \
-    -v "$(pwd)/../../react-ui/node_modules:/app/node_modules:cached" \
-    -v "$(pwd)/../../react-ui/public/:/app/public/:ro,cached" \
-    -v "$(pwd)/../../react-ui/src/:/app/src/:cached" \
-    -v "$(pwd)/../../react-ui/images.d.ts:/app/images.d.ts:ro,cached" \
-    -v "$(pwd)/../../react-ui/package.json:/app/package.json:cached" \
-    -v "$(pwd)/../../react-ui/tsconfig.json:/app/tsconfig.json:ro,cached" \
-    -v "$(pwd)/../../react-ui/tsconfig.prod.json:/app/tsconfig.prod.json:ro,cached" \
-    -v "$(pwd)/../../react-ui/tsconfig.test.json:/app/tsconfig.test.json:ro,cached" \
-    -v "$(pwd)/../../react-ui/tslint.json:/app/tslint.json:ro,cached" \
-    -v "$(pwd)/../../react-ui/yarn.lock:/app/yarn.lock:cached" \
-    -v "$(pwd)/../../react-ui/yarn-error.log:/app/yarn-error.log:cached" \
-    -v "$(pwd)/../../core/src/main/resources/react-ui-compiled/:/target-output/react-ui-compiled:cached" \
-    -v "$(pwd)/../../tmp/react-ui/.yarn-cache:/app/.yarn-cache:cached" \
+DOCKER_ARGS=(-e "COMMAND=$COMMAND")
+
+if [[ "$COMMAND" = "start" ]] || [[ "$COMMAND" = 'bash' ]]; then
+    DOCKER_ARGS+=(-it -p 3000:3000 -v "$(pwd)/../../tmp/react-ui/.yarn-cache:/app/.yarn-cache:cached")
+fi
+
+DOCKER_ARGS+=(-v "$(pwd)/../../react-ui/.storybook/:/app/.storybook/:ro,cached")
+DOCKER_ARGS+=(-v "$(pwd)/../../react-ui/node_modules:/app/node_modules:cached")
+DOCKER_ARGS+=(-v "$(pwd)/../../react-ui/public/:/app/public/:ro,cached")
+DOCKER_ARGS+=(-v "$(pwd)/../../react-ui/src/:/app/src/:cached")
+DOCKER_ARGS+=(-v "$(pwd)/../../react-ui/images.d.ts:/app/images.d.ts:ro,cached")
+DOCKER_ARGS+=(-v "$(pwd)/../../react-ui/package.json:/app/package.json:cached")
+DOCKER_ARGS+=(-v "$(pwd)/../../react-ui/tsconfig.json:/app/tsconfig.json:ro,cached")
+DOCKER_ARGS+=(-v "$(pwd)/../../react-ui/tsconfig.prod.json:/app/tsconfig.prod.json:ro,cached")
+DOCKER_ARGS+=(-v "$(pwd)/../../react-ui/tsconfig.test.json:/app/tsconfig.test.json:ro,cached")
+DOCKER_ARGS+=(-v "$(pwd)/../../react-ui/tslint.json:/app/tslint.json:ro,cached")
+DOCKER_ARGS+=(-v "$(pwd)/../../react-ui/yarn.lock:/app/yarn.lock:cached")
+DOCKER_ARGS+=(-v "$(pwd)/../../react-ui/yarn-error.log:/app/yarn-error.log:cached")
+DOCKER_ARGS+=(-v "$(pwd)/../../core/src/main/resources/react-ui-compiled/:/target-output/react-ui-compiled:cached")
+
+echo "running docker container ..."
+docker run --rm \
+    "${DOCKER_ARGS[@]}" \
     config-doc-gen/react-ui-local-dev
