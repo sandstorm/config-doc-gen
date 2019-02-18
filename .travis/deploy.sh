3f41aea55d31b641b8c6f9b870b2c8a9ed7b5d56
Eric Kloss <eric.kloss@sandstorm.de>
2019-02-18 15:28:02 +0100

    fix travis build

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,10 +3,10 @@
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ];
 then
     echo "Sign and Upload Archives"
-    ./gradlew :annotations:publish :core:publish :processor-standalone:publish :processor-spring:publish
+    ./gradlew publish
     if [ "$MANUAL_RELEASE_TRIGGERED" = "true" ];
     then
         echo "Promote repository"
-        ./gradlew :annotations:closeAndReleaseRepository :core:closeAndReleaseRepository :processor-spring:closeAndReleaseRepository :processor-standalone:closeAndReleaseRepository
+        ./gradlew closeAndReleaseRepository
     fi
 fi
